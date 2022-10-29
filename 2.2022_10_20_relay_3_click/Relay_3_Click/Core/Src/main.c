6a468b4495158a30d4757b21e6182e9e14ce4aa5
Tan-No Nguyen <44743864+NGUYENTANNO@users.noreply.github.com>
2022-10-29 17:26:13 +0700

    messa

---
+++ 2.2022_10_20_relay_3_click/Relay_3_Click/Core/Src/main.c
@@ -0,0 +1,247 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "math.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void bitWrite(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
+{
+	if(PinState == 1)
+		GPIOx->BSRR= (uint32_t)pow(2,GPIO_Pin);
+	else
+		GPIOx->BSRR = (uint32_t)pow(2,GPIO_Pin)<<16U;
+}	
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+	/***********Enable clock PORTE, PORTA**********
+	***********************************************
+	***********************************************/
+	RCC->AHB1ENR =  (1<<4);  	  //GPIOE clock enable
+	RCC->AHB1ENR |= (1<<0);  		//GPIOA clock enable
+	/***********EGPIO Config for ******************
+	******PE0, 	PA10		: input pull-up for button***
+	******PE12, PE15 		: Output for led*************
+	******PE7, 	PA12		: Output for control 2 relay*
+	*************************************************/
+	//////////////////////INPUT//////////////////////////////
+	GPIOE->MODER |= (0<<0);  	// pin PE0(bits 1:0 as intput (00))
+	GPIOA->MODER |= (0<<20); 	// pin PA10(bits 21:20 as intput (00))
+	GPIOE->PUPDR |= (1<<0);		// pin PE0(bits 1:0 as pull-up (01))
+	GPIOA->PUPDR |= (1<<20);	// pin PA10(bits 21:20 as pull-up (01))
+	////////////////////////OUTPUT///////////////////////////
+	GPIOE->MODER |= (1<<24);  // pin PE12(bits 25:24) is Output (01)
+	GPIOE->MODER |= (1<<30); 	// pin PE15(bits 31:30) is Output (01)
+	////////////////////////OUTPUT//////////////////////////
+	GPIOE->MODER |=  (1<<14);  // pin PE7(bits 18:17) is Output (01)
+	GPIOE->MODER |= (1<<16); 	// pin PE8(bits 17:16) is Output (01)	
+  /* USER CODE END 2 */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+		
+	//bitWrite(GPIOE,12,0);
+		HAL_GPIO_WritePin(GPIOE,GPIO_PIN_12,0);
+		HAL_GPIO_WritePin(GPIOE,1<<15,1);
+		HAL_GPIO_WritePin(GPIOE,1<<7,1);
+		HAL_GPIO_WritePin(GPIOE,1<<8,0);
+		if(HAL_GPIO_ReadPin(GPIOA,1<<10)==0)
+		 HAL_Delay(100);
+		else if (HAL_GPIO_ReadPin(GPIOE,1<<0)==0)
+			HAL_Delay(500);
+		else
+			HAL_Delay(1000);		
+	//bitWrite(GPIOE,12,1);	
+		HAL_GPIO_WritePin(GPIOE,GPIO_PIN_12,1);
+		HAL_GPIO_WritePin(GPIOE,1<<15,0);
+		HAL_GPIO_WritePin(GPIOE,1<<7,0);
+		HAL_GPIO_WritePin(GPIOE,1<<8,1);
+		if(HAL_GPIO_ReadPin(GPIOA,1<<10)==0)
+		 HAL_Delay(100);
+		else if (HAL_GPIO_ReadPin(GPIOE,1<<0)==0)
+			HAL_Delay(500);
+		else
+			HAL_Delay(1000);
+
+
+
+		//	HAL_GPIO_WritePin(
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ 2.2022_10_20_relay_3_click/Relay_3_Click/MDK-ARM/RTE/_Relay_3_Click/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Relay_3_Click' 
+ * Target:  'Relay_3_Click' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
